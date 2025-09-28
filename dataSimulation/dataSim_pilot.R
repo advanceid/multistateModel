297a1deb8ab7a1f21b8c98296893e0abf0d4d12a
huangxy13 <cepifh@gmail.com>
2025-09-28 18:53:30 +0800

    NecTAR sample size

--- dataSimulation/dataSim_pilot.R
+++ dataSimulation/dataSim_pilot.R
@@ -1,6 +1,6 @@
 # Preparation ----
 rm(list = ls())
-
+# setwd('D:/github/multistateModel')
 setwd('dataSimulation')
 print(getwd())
 set.seed(908)
@@ -68,20 +68,21 @@ tmat <- matrix(ncol = 5, byrow = T,
                  0, a42, a43, -a42-a43-a45, a45,
                  0, 0, 0, 0, 0))
 
-probTrans <- expm(tmat)
-probTrans
-probTrans <- data.frame(probTrans) %>%
-  rename(prob1 = X1, prob2 = X2, prob3 = X3, prob4 = X4, prob5 = X5) %>%
-  mutate(state0 = 1:5)
+# probTrans <- expm(tmat)
+# probTrans
+# probTrans <- data.frame(probTrans) %>%
+#   rename(prob1 = X1, prob2 = X2, prob3 = X3, prob4 = X4, prob5 = X5) %>%
+#   mutate(state0 = 1:5)
+# 
+# probObs <- matrix(c(1, 0, 0, 0, 0,
+#                   0.05, 0.95, 0, 0, 0,
+#                   0.05, 0, 0.95, 0, 0,
+#                   0.01, 0.05, 0.05, 0.89, 0,
+#                   0, 0, 0, 0, 1), ncol = 5, byrow = T)
+# probObs <- data.frame(probObs) %>%
+#   rename(probO1 = X1, probO2 = X2, probO3 = X3, probO4 = X4, probO5 = X5) %>%
+#   mutate(stateNew = 1:5)
 
-probObs <- matrix(c(1, 0, 0, 0, 0,
-                  0.05, 0.95, 0, 0, 0,
-                  0.05, 0, 0.95, 0, 0,
-                  0.01, 0.05, 0.05, 0.89, 0,
-                  0, 0, 0, 0, 1), ncol = 5, byrow = T)
-probObs <- data.frame(probObs) %>%
-  rename(probO1 = X1, probO2 = X2, probO3 = X3, probO4 = X4, probO5 = X5) %>%
-  mutate(stateNew = 1:5)
 
 
 # cohort initiation ----
@@ -100,142 +101,41 @@ cohort %<>% mutate(
   dplyr::select(!age2)
 
 # cohort update ----
-write.csv(cohort, 'store/cohortDay000000.csv', row.names = F)
-
-
-#### starting from middle ####
-existing <- dir('store', full.names = T)
-print(existing)
-if (length(existing) > 2){
-  existing <- existing[length(existing) - 1]
-  breaking <- str_extract(existing, '[0-9]+')
-  breaking <- as.numeric(breaking)
-} else{
-  breaking = 1
-  existing <- existing[1]
-}
-cohort <- read.csv(existing)
+write.csv(cohort, 'cohortInit.csv', row.names = F)
 
 
 
-#### Parallelization ####
-start <- Sys.time()
-ncores <- as.integer(Sys.getenv("PBS_NCPUS", unset = detectCores()))
-ncores <- min(ncores, 100)
-print(ncores)
-cl <- makeCluster(ncores)
-
-
-
-#### Update ####
-updateC <- function(cohort = cohort){
-  library(tidyverse)
-  library(magrittr)
+# Data simulation ----
+# for(subj in 1:nrow(cohort)){
+history <- data.frame(id = NA, time0 = NA, time1 = NA, state0 = NA, state1 = NA)
+for(subj in 1:nrow(cohort)){
+  id <- cohort$id[subj]
+  t0 <- 0
+  # tTrans <- 0
+  state0 <- cohort$state0[subj]
   
-  cohort <- cohort %>% left_join(probTrans, by = 'state0') %>%
-    rowwise() %>%
-    mutate(day = i,
-           age = age + 1/365,
-           rollT = runif(1, 0, 1),
-           stateNew = 1 + (rollT > prob1) + (rollT > prob1 + prob2)  + 
-             (rollT > prob1 + prob2 + prob3) + 
-             (rollT > prob1 + prob2 + prob3 + prob4),
-           stay = ifelse(stateNew == state0, stay + 1, 0),
-           
-           probVisit = ifelse(visit == 0,
-                              recode(stateNew, `1` = 0.1, `2` = 40, `3` = 20,
-                                     `4` = 50, `5` = 90),
-                              recode(stateO, `1` = 0.1, `2` = 60, `3` = 40,
-                                     `4` = 80, `5` = 100)),
-           rollV = runif(1, 0, 1),
-           visitNew = rollV < probVisit/100) %>%
-    left_join(probObs, by = 'stateNew') %>%
-    mutate(rollO = runif(1, 0, 1),
-      stateO = 1 + (rollO > probO1) + (rollO > probO1 + probO2)  +
-             (rollO > probO1 + probO2 + probO3) +
-             (rollO > probO1 + probO2 + probO3 + probO4)) %>%
-    ungroup %>% dplyr::select(
-      -c(prob1:prob5, probO1:probO5, rollT, rollV, rollO))
-  
-  return(cohort)
+  while((t0 < nDays)&(state0 < 5)){
+    lambda <- -tmat[state0,state0]
+    tTrans <- rexp(1, rate = lambda)
+    
+    Ptrans <- expm(tmat*tTrans)
+    Ptrans <- Ptrans[state0, ]
+    Ptrans[state0] <- 0
+    Ptrans <- Ptrans/sum(Ptrans)
+    state1 <- sample(1:5, size = 1, prob = Ptrans)
+    
+    temp <- data.frame(
+      id = id,
+      time0 = t0, 
+      time1 = t0 + tTrans,
+      state0 = state0,
+      state1 = state1)
+      
+    history <- rbind(history, temp)
+    
+    state0 <- state1
+    t0 <- t0 + tTrans
+  }
 }
 
-# Execute in parallel
-for(i in breaking:nDays){
-  print(i)
-  clusterExport(cl, varlist = c("probTrans", 'probObs', 'i'))
-  cohorts <- split(cohort, cut(cohort$id, ncores, labels = FALSE))
-  cohorts <- parLapply(cl, cohorts, updateC)
-
-  cohort <- do.call(rbind, cohorts)
-  write.csv(cohort, row.names = F,
-            paste0('store/cohortDay', str_pad(i, 7, pad = '0'), '.csv'))
-  if (length(unique(cohort$id)) != nrow(cohort)) break
-  cohort %<>% mutate(state0 = stateNew, visit = visitNew)
-}
-stopCluster(cl)
-end <- Sys.time()
-
-print(end - start)
-
-
-
-# #### Comparison ####
-# start2 <- Sys.time()
-# for(i in 1:2){
-# cohort <- cohort %>% left_join(probTrans, by = 'state0') %>%
-#   rowwise() %>%
-#   mutate(day = i,
-#          age = age + 1/365,
-#          rollT = runif(1, 0, 1),
-#          stateNew = 1 + (rollT > prob1) + (rollT > prob1 + prob2)  + 
-#            (rollT > prob1 + prob2 + prob3) + 
-#            (rollT > prob1 + prob2 + prob3 + prob4),
-#          stay = ifelse(stateNew == state0, stay + 1, 0),
-#          
-#          probVisit = ifelse(visit == 0,
-#                             recode(stateNew, `1` = 1, `2` = 40, `3` = 30,
-#                                    `4` = 50, `5` = 90),
-#                             recode(stateNew, `1` = 10, `2` = 80, `3` = 60,
-#                                    `4` = 90, `5` = 100)),
-#          rollV = runif(1, 0, 1),
-#          visitNew = rollV < probVisit/100) %>%
-#   left_join(probObs, by = 'stateNew') %>%
-#   mutate(rollO = runif(1, 0, 1),
-#          stateO = 1 + (rollO > probO1) + (rollO > probO1 + probO2)  +
-#            (rollO > probO1 + probO2 + probO3) +
-#            (rollO > probO1 + probO2 + probO3 + probO4)) %>%
-#   ungroup %>% dplyr::select(
-#     -c(prob1:prob5, probO1:probO5, rollT, rollV, rollO))
-# }
-# end2 <- Sys.time()
-# 
-# 
-# # cohort update ----
-# sample_row <- function(prob_row){ sample(1:5, size = 1, prob = prob_row)}
-# 
-# for (i in 1:nDays){
-#   cohort <- cohort %>% left_join(dfTrans, by = 'state0') %>%
-#     rowwise() %>%
-#     mutate(stateNew = sample_row(c_across(prob1:prob5)),
-#            day = i,
-#            age = age + 1/365,
-#            stay = ifelse(stateNew == state0, stay + 1, 0),
-#            probVisit = ifelse(visit == 0,
-#                               recode(stateNew, `1` = 1, `2` = 40, `3` = 30, 
-#                                      `4` = 50, `5` = 90),
-#                               recode(stateNew, `1` = 10, `2` = 80, `3` = 60, 
-#                                      `4` = 90, `5` = 100)),
-#            probVisit = probVisit/100,
-#            visitNew = rbinom(1, size = 1, prob = probVisit)) %>%
-#     left_join(dfObs, by = 'stateNew') %>%
-#     mutate(stateO = sample_row(c_across(probO1:probO5))) %>%
-#     ungroup %>% dplyr::select(
-#       -c(prob1:prob5, probO1:probO5))
-#   write.csv(cohort, paste0('store/cohortDay', str_pad(i, 7, pad = '0'), '.csv'),
-#             row.names = F)
-#   cohort %<>% mutate(state0 = stateNew, visit = visitNew)
-#   }
-# 
-# 
-# 
+write.csv(history, 'history.csv', row.names = F)
